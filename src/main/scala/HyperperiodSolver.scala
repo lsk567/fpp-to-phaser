d4204fcc8f5505dc289bd5ef3ca301042be93105
Shaokai Lin <shaokai@berkeley.edu>
2025-07-26 13:38:11 -0700

    Work on next and hpCheck

--- src/main/scala/HyperperiodSolver.scala
+++ src/main/scala/HyperperiodSolver.scala
@@ -4,27 +4,22 @@ import fpp.compiler.ast._
 
 object HyperperiodSolver {
 
+    /** A port call consists of a rate group (caller) and a port instance (callee). */
+    type PortCall = (Symbol.ComponentInstance, PortInstanceIdentifier)
 
-    // Timestamp, current set of invocations, a map of future timestamps to future invocations. 
-    type Step = (Time, Set[PortInstanceIdentifier], Map[Time, Set[PortInstanceIdentifier]])
-    
     /**
-      * To compute the next step, take the earliest events from
-      * the pending events list, set their 
-      *
-      * @return
+      * A step consists of a timestamp, a set of port calls,
+      * a map of future timestamps to future port calls. 
       */
-    // def next(s: Step): Step = {
+    type Step = (Time, Set[PortCall], Map[Time, Set[PortCall]])
 
-    // }
-
-    // hp = hyperperiod
     @annotation.tailrec
     def solve(
+        analysis: PhaserAnalysis,
         current: Step,
         trace: List[Step]
     )(
-        next: Step => Step,
+        next: (PhaserAnalysis, Step) => Step,
         hpCheck: (Step, List[Step]) => Option[Int]
     ): (List[Step], Int) = {
         hpCheck(current, trace) match {
@@ -32,7 +27,75 @@ object HyperperiodSolver {
                 val fullTrace = (current :: trace).reverse
                 (fullTrace, index)
             case None =>
-                solve(next(current), current :: trace)(next, hpCheck)
+                solve(analysis, next(analysis, current), current :: trace)(next, hpCheck)
         }
     }
+
+    /**
+      * The next step's timestamp and port calls are the earliest entry
+      * of the pending events map. Then remove the entry from the map and
+      * add new pending events to the map based on the current invocations.  
+      */
+    def next(a: PhaserAnalysis, step: Step): Step = {
+        val (_, _, currentMap) = step
+        // Get the next time and port calls.
+        val (nextTime, nextCalls): (Time, Set[PortCall]) = currentMap.minBy(_._1)
+        // Infer calls beyond the next time.
+        val futureCalls: Set[(Time, PortCall)] =
+            nextCalls.map(call => {
+                // For each call, find the caller rate group's period,
+                // add to the current time.
+                val (rateGroup, _) = call
+                val period = a.periodMap(rateGroup)
+                val futureTime = nextTime + period
+                (futureTime, call)
+            })        
+        // Create the next map using future calls.
+        val nextMapInit = currentMap.removed(nextTime)
+        val nextMap = futureCalls.foldLeft(nextMapInit)((map, timedCall) => {
+            val (time, call) = timedCall
+            map.updatedWith(time) {
+                case Some(set) => Some(set + call)
+                case None => Some(Set(call))
+            }
+        })
+        (nextTime, nextCalls, nextMap)
+    }
+
+    /**
+      * Given a new step and an existing trace, check if the step
+      * has occured in the trace before. If so, return the index of recurrence.
+      * 
+      * A step s matches a prior step s' in the trace when they have the same port calls,
+      * and all entries in the map have the same time interval relative to
+      * the timestamp of the step.
+      * 
+      * Specially, denote the current time as t, the previous time as t', the maps m and m'.
+      * For an entry e in m, where e = (time -> Set(calls)),
+      * we check if there exists an e' = (time' -> Set(calls')) in m',
+      * such that calls = calls' and time - t = time' - t'. If this holds for all entries in m,
+      * then we say step s and s' "match".
+      *
+      * @param step
+      * @param trace
+      */
+    def hpCheck(step: Step, trace: List[Step]): Option[Int] = {
+        val (t, calls, m) = step
+
+        trace.zipWithIndex.find { case ((tPrev, callsPrev, mPrev), idx) =>
+            // First check if the current port calls match the previous step's port calls
+            if (calls != callsPrev) false
+            else {
+                // Normalize both maps by shifting their time keys relative to their respective timestamps
+                def normalizeMap(baseTime: Time, map: Map[Time, Set[PortCall]]): Map[BigInt, Set[PortCall]] =
+                    map.map { case (time, callSet) => (time.toNanoseconds - baseTime.toNanoseconds) -> callSet }
+
+                val normM     = normalizeMap(t, m)
+                val normMPrev = normalizeMap(tPrev, mPrev)
+
+                normM == normMPrev
+            }
+        }.map(_._2) // Return the index of the matching step
+    }
+        
 }
--- src/main/scala/Time.scala
+++ src/main/scala/Time.scala
@@ -1,6 +1,26 @@
 package fpp.compiler.analysis
 
-sealed trait Time
-case class MS(value: BigInt) extends Time
-case class US(value: BigInt) extends Time
-case class NS(value: BigInt) extends Time
+sealed trait Time {
+  def toNanoseconds: BigInt
+
+  def +(that: Time): Time = Time.fromNanoseconds(this.toNanoseconds + that.toNanoseconds)
+  def -(that: Time): Time = Time.fromNanoseconds(this.toNanoseconds - that.toNanoseconds)
+}
+
+case class MS(value: BigInt) extends Time {
+  def toNanoseconds: BigInt = value * 1_000_000
+}
+
+case class US(value: BigInt) extends Time {
+  def toNanoseconds: BigInt = value * 1_000
+}
+
+case class NS(value: BigInt) extends Time {
+  def toNanoseconds: BigInt = value
+}
+
+object Time {
+  implicit val timeOrdering: Ordering[Time] = Ordering.by(_.toNanoseconds)
+
+  def fromNanoseconds(ns: BigInt): Time = NS(ns)
+}
